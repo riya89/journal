a8a4f89aac5345d0aaeb0cbd0e9878f5fc67fe57
Riya Banik <riyabanik@Riyas-MacBook-Air.local>
2025-11-13 00:14:34 +0530

    added photo of the day feature

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ import promptRoutes from "./routes/prompts.js";
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.use("/auth", authRoutes);
 app.use("/journal", journalRoutes);
 app.use("/prompts", promptRoutes);
